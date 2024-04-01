d846fa7bf3e77b89e45e21b86e4ec92d0cc3a182
CarlosAsiB <146340578+CarlosAsiB@users.noreply.github.com>
2024-03-31 21:04:02 -0300

    Update NavBar.js
    Removiendo cosas innecesarias y codigo sin uso

--- clase2/src/components/NavBar.js
+++ clase2/src/components/NavBar.js
@@ -1,4 +1,3 @@
-// NavBar.js
 import { Link } from "react-router-dom";
 import React, { useContext } from 'react';
 import { CartContext } from '../context/CartContext'; 
@@ -6,9 +5,7 @@ import CartWidget from '../components/CartWidget';
 import CarritoImg from ".././components/img/Carrito.png";
 
 function NavBar() {
-  const { cart } = useContext(CartContext);
-  const itemCount = cart.reduce((total, cartItem) => total + cartItem.quantity, 0);
-
+  
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
@@ -20,7 +17,6 @@ function NavBar() {
       <div className="navbar-end">
         <div className="navbar-item">
           <CartWidget img={CarritoImg} />
-          {itemCount > 0 && <span>{itemCount}</span>}
         </div>
       </div>
     </nav>
